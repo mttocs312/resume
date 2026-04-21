11ae6533993efd3775f11c06fa8e4f400c6b439b
Scott Milewski <scottmilewski@yahoo.com>
2026-04-21 12:17:58 -0500

    invalidate cloudfront cache in script

--- ci/s3SyncMainApplication.sh
+++ ci/s3SyncMainApplication.sh
@@ -5,3 +5,7 @@ DIST_DIR="./out"
 BUCKET_NAME="www.scottmilewski.com"
 
 aws s3 sync $DIST_DIR s3://$BUCKET_NAME
+
+aws cloudfront create-invalidation --distribution-id E2U6HGRYYW64NC --paths "/*"
+
+aws cloudfront create-invalidation --distribution-id E3BVWNPAAF8BDR --paths "/*"
